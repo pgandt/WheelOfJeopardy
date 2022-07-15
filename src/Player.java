2a498df96eba8dd9c94640d8c9a5af22fb2e122a
mgoular2 <109384938+mgoular2@users.noreply.github.com>
2022-07-15 15:19:36 -0400

    added 2 methods to set and get the current player

--- src/Player.java
+++ src/Player.java
@@ -4,19 +4,29 @@ public class Player {
     private int score;
     private String name;
     private int freeTurns;
-    public nextPlayer;
+    public int nextPlayer;
     
     public Player(String name) {
         this.score = 0;
         this.name = name;
         this.freeTurns = 0;
     }
-
-    //maybe update score?
+    
+    //set Player
+    public void setPlayer(int newPlayer){
+        this.nextPlayer = nextPlayer;
+    }
+    
+    //get Player
+    public int getPlayer() {
+        return this.nextPlayer;
+    }
+       
+    //set Score
     public void setScore(int newScore){
         this.score = score;
     }
-
+    //get Score
     public int getScore() {
         return this.score;
     }
